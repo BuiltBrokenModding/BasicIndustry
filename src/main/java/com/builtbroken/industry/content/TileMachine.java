da54d55c8ea8d1243eb2f79c850507fbcd0b5969
Robert S <rseifert.phone@gmail.com>
2015-01-12 21:46:52 -0500

    Implemented module system for machine classes

--- src/main/java/com/builtbroken/industry/content/TileMachine.java
+++ src/main/java/com/builtbroken/industry/content/TileMachine.java
@@ -8,6 +8,7 @@ import com.builtbroken.mc.core.network.packet.AbstractPacket;
 import com.builtbroken.mc.core.network.packet.PacketTile;
 import com.builtbroken.mc.prefab.inventory.InventoryUtility;
 import com.builtbroken.mc.prefab.tile.Tile;
+import com.builtbroken.mc.prefab.tile.TileModuleMachine;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
@@ -20,19 +21,8 @@ import net.minecraft.nbt.NBTTagList;
  * Prefab for electric machines, will move to core when completed
  * Created by robert on 1/7/2015.
  */
-public abstract class TileMachine extends Tile implements ISidedInventory, IPacketIDReceiver, IGuiTile
+public abstract class TileMachine extends TileModuleMachine implements IPacketIDReceiver, IGuiTile
 {
-    /**
-     * Size of the inventory
-     */
-    protected int inventory_size;
-    /**
-     * Slots that can be accessed all sides, defaults to all slots
-     */
-    protected int[] openSlots;
-
-    //Vars that are accessed with setters
-    private ItemStack[] containedItems;
     private boolean enabled = false;
     private boolean prev_enabled = false;
     private OnOffOption onOffOption = OnOffOption.AUTOMATIC;
@@ -40,8 +30,8 @@ public abstract class TileMachine extends Tile implements ISidedInventory, IPack
 
     public TileMachine(Material material, int inventory_size)
     {
-        super(material);
-        this.inventory_size = inventory_size;
+        super("", material);
+        this.addInventoryModule(inventory_size);
     }
 
     //==============================
@@ -154,200 +144,18 @@ public abstract class TileMachine extends Tile implements ISidedInventory, IPack
         return false;
     }
 
-    //==============================
-    //======= Inventory Code =======
-    //==============================
-
-    //TODO move inventory code to its own prefab
-    @Override
-    public int getSizeInventory()
-    {
-        return inventory_size;
-    }
-
-    @Override
-    public ItemStack getStackInSlot(int slot)
-    {
-        if (slot < getContainedItems().length)
-        {
-            return this.getContainedItems()[slot];
-        }
-        return null;
-    }
-
-    @Override
-    public ItemStack decrStackSize(int slot, int ammount)
-    {
-        if (this.getContainedItems()[slot] != null)
-        {
-            ItemStack var3;
-
-            if (this.getContainedItems()[slot].stackSize <= ammount)
-            {
-                var3 = this.getContainedItems()[slot];
-                getContainedItems()[slot] = null;
-                markDirty();
-                return var3;
-            }
-            else
-            {
-                var3 = this.getContainedItems()[slot].splitStack(ammount);
-
-                if (this.getContainedItems()[slot].stackSize == 0)
-                {
-                    this.getContainedItems()[slot] = null;
-                }
-
-                markDirty();
-                return var3;
-            }
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    @Override
-    public ItemStack getStackInSlotOnClosing(int par1)
-    {
-        if (this.getContainedItems()[par1] != null)
-        {
-            ItemStack var2 = this.getContainedItems()[par1];
-            this.getContainedItems()[par1] = null;
-            return var2;
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    @Override
-    public void setInventorySlotContents(int slot, ItemStack insertStack)
-    {
-        ItemStack pre_stack = this.getContainedItems()[slot] != null ? this.getContainedItems()[slot].copy() : null;
-        this.getContainedItems()[slot] = insertStack;
-
-        if (insertStack != null && insertStack.stackSize > this.getInventoryStackLimit())
-        {
-            insertStack.stackSize = this.getInventoryStackLimit();
-        }
-        if (!InventoryUtility.stacksMatchExact(pre_stack, getContainedItems()[slot]))
-        {
-            markDirty();
-        }
-    }
-
-    @Override
-    public boolean hasCustomInventoryName()
-    {
-        return true;
-    }
-
-    @Override
-    public String getInventoryName()
-    {
-        return "container." + BasicIndustry.PREFIX + name;
-    }
-
-    @Override
-    public void openInventory()
-    {
-
-    }
-
-    @Override
-    public void closeInventory()
-    {
-
-    }
-
-    @Override
-    public boolean isItemValidForSlot(int i, ItemStack itemstack)
-    {
-        return i <= this.getSizeInventory();
-    }
-
-    @Override
-    public int[] getAccessibleSlotsFromSide(int var1)
-    {
-        if (openSlots == null || openSlots.length != this.getSizeInventory())
-        {
-            this.openSlots = new int[this.getSizeInventory()];
-            for (int i = 0; i < this.openSlots.length; i++)
-            {
-                openSlots[i] = i;
-            }
-        }
-        return this.openSlots;
-    }
-
-    @Override
-    public int getInventoryStackLimit()
-    {
-        return 64;
-    }
-
-    @Override
-    public boolean isUseableByPlayer(EntityPlayer par1EntityPlayer)
-    {
-        return true;
-    }
-
-    public ItemStack[] getContainedItems()
-    {
-        if (this.containedItems == null)
-        {
-            this.containedItems = new ItemStack[this.getSizeInventory()];
-        }
-        return this.containedItems;
-    }
-
     @Override
     public void readFromNBT(NBTTagCompound nbt)
     {
         super.readFromNBT(nbt);
-        this.clear();
-
-        NBTTagList nbtList = nbt.getTagList("Items", 10);
-
-        for (int i = 0; i < nbtList.tagCount(); ++i)
-        {
-            NBTTagCompound stackTag = nbtList.getCompoundTagAt(i);
-            byte id = stackTag.getByte("Slot");
-
-            if (id >= 0 && id < this.getSizeInventory())
-            {
-                this.setInventorySlotContents(id, ItemStack.loadItemStackFromNBT(stackTag));
-            }
-        }
+        onOffOption = OnOffOption.get(nbt.getInteger("onOffState"));
     }
 
     @Override
     public void writeToNBT(NBTTagCompound nbt)
     {
         super.writeToNBT(nbt);
-        NBTTagList nbtList = new NBTTagList();
-
-        for (int i = 0; i < this.getSizeInventory(); ++i)
-        {
-            if (this.getStackInSlot(i) != null)
-            {
-                NBTTagCompound var4 = new NBTTagCompound();
-                var4.setByte("Slot", (byte) i);
-                this.getStackInSlot(i).writeToNBT(var4);
-                nbtList.appendTag(var4);
-            }
-        }
-
-        nbt.setTag("Items", nbtList);
-    }
-
-    public void clear()
-    {
-        this.containedItems = null;
-        this.getContainedItems();
+        nbt.setInteger("onOffState", onOffOption.ordinal());
     }
 
     public static enum OnOffOption
