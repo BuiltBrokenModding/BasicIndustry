916a43dd1aa328eeb1360a0e27c4833319541f5c
Robert Seifert <rseifert.phone@gmail.com>
2016-01-20 01:54:52 -0500

    Reminders for later

--- src/main/java/com/builtbroken/industry/content/machines/container/TileResourcePile.java
+++ src/main/java/com/builtbroken/industry/content/machines/container/TileResourcePile.java
@@ -1,11 +1,11 @@
 package com.builtbroken.industry.content.machines.container;
 
 /** #MultiBlock
- * Fluid tank like silo that can store large amounts of dust items in a visual block like way.
+ * Visual based stockpile of raw resources. Examples ore, cobble stone, stone blocks, slabs, etc.
  *
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 1/20/2016.
  */
-public class TileDustSilo
+public class TileResourcePile
 {
 }
