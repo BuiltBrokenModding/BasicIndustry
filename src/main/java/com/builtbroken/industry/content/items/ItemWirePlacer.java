6770c1e17590e5240f5bed36551cdf7696d96021
Robert Seifert <rseifert.phone@gmail.com>
2016-06-13 06:41:37 -0400

    Reminder for later

---
+++ src/main/java/com/builtbroken/industry/content/items/ItemWirePlacer.java
@@ -0,0 +1,9 @@
+package com.builtbroken.industry.content.items;
+
+/** Used to place basic wires in different ways, including floor wires(redstone wire visual), ceiling wires(redstone wire visual), flat(RI wires), full block(IC2 style), wall hanging(IC2 style with mounting brackets), micro block(Tray style), etc
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 6/5/2016.
+ */
+public class ItemWirePlacer
+{
+}
